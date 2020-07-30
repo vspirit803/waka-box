5c842dc51c96678d10b8551095106633daca7cb5
vspirit803 <lx546488173@gmail.com>
2020-07-30 23:11:59 +0800

    wip: featch durations

--- fetchData.js
+++ fetchData.js
@@ -1,18 +1,49 @@
 require("dotenv").config();
 const { WakaTimeClient, RANGE } = require("wakatime-client");
-
-const { WAKATIME_API_KEY: wakatimeApiKey } = process.env;
+const MongoClient = require("mongodb").MongoClient;
+const { WAKATIME_API_KEY: wakatimeApiKey, MONGODB_URL: url } = process.env;
 
 const wakatime = new WakaTimeClient(wakatimeApiKey);
 
-console.log(wakatimeApiKey);
+async function connectDb() {
+  return new Promise((resolve, reject) => {
+    MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+      if (err) {
+        reject(err);
+      }
+      console.log("数据库已连接!");
+      resolve(db);
+    });
+  });
+}
 
 async function main() {
   // const stats = await wakatime.getMyStats({ range: RANGE.LAST_7_DAYS });
   // console.log(stats)
+  const db = await connectDb().catch(err => {
+    console.log(err);
+  });
+  const collection = db.db("projectTime").collection("durations");
+  const date = "2020-07-30";
+  const durationsRaw = await wakatime.getMyDurations({ date });
+  const durations = durationsRaw.data.map(
+    ({ created_at, duration, id, machine_name_id, project, time }) => ({
+      created_at,
+      duration,
+      id,
+      machine_name_id,
+      project,
+      time,
+      date
+    })
+  );
+  console.log(durations);
+  collection.insertMany(durations, function(err, res) {
+    if (err) throw err;
+    console.log("插入的文档数量为: " + res.insertedCount);
+  });
 
-  const stats = await wakatime.getMyDurations({ date: "2020-07-30" });
-  console.log(stats);
+  db.close();
 }
 
 (async () => {
