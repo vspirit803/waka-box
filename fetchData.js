ab718f3a0db2f89b1327312653a0c23b6c1956df
vspirit803 <52484564+vspirit803@users.noreply.github.com>
2020-07-31 22:00:47 +0800

    wip: fetch languages

--- fetchData.js
+++ fetchData.js
@@ -18,32 +18,67 @@ async function connectDb() {
 }
 
 async function main() {
-  // const stats = await wakatime.getMyStats({ range: RANGE.LAST_7_DAYS });
-  // console.log(stats)
-  const db = await connectDb().catch(err => {
+  const dbConnect = await connectDb().catch(err => {
     console.log(err);
   });
-  const collection = db.db("projectTime").collection("durations");
+  const db = dbConnect.db("projectTime");
+  // const collection = db.collection("durations");
   const date = "2020-07-30";
-  const durationsRaw = await wakatime.getMyDurations({ date });
-  const durations = durationsRaw.data.map(
-    ({ created_at, duration, id, machine_name_id, project, time }) => ({
-      created_at,
-      duration,
-      id,
-      machine_name_id,
-      project,
-      time,
+  // const durationsRaw = await wakatime.getMyDurations({ date });
+  // const durations = durationsRaw.data.map(
+  //   ({ created_at, duration, id, machine_name_id, project, time }) => ({
+  //     created_at,
+  //     duration,
+  //     id,
+  //     machine_name_id,
+  //     project,
+  //     time,
+  //     date,
+  //   })
+  // );
+  // console.log(durations);
+  // if (durations && durations.length) {
+  //   db.collection("durations").insertMany(durations, function(err, res) {
+  //     if (err) throw err;
+  //     console.log("插入的durations数量为: " + res.insertedCount);
+  //   });
+  // }
+
+  const summaryRaw = await wakatime.getMySummary({
+    dateRange: { startDate: date, endDate: date }
+  });
+  const languages = summaryRaw.data?.[0]?.languages?.map(
+    ({ name, percent, total_seconds }) => ({
+      name,
+      percent,
+      total_seconds,
       date
     })
   );
-  console.log(durations);
-  collection.insertMany(durations, function(err, res) {
-    if (err) throw err;
-    console.log("插入的文档数量为: " + res.insertedCount);
-  });
+  if (languages && languages.length) {
+    db.collection("languages").insertMany(languages, function(err, res) {
+      if (err) throw err;
+      console.log("插入的languages数量为: " + res.insertedCount);
+    });
+  }
 
-  db.close();
+  //项目统计
+  // const projects = summaryRaw.data?.[0]?.projects?.map(
+  //   ({ name, percent, total_seconds }) => ({
+  //     name,
+  //     percent,
+  //     total_seconds,
+  //     date,
+  //   })
+  // );
+  // if (languages && languages.length) {
+  //   db.collection("languages").insertMany(languages, function(err, res) {
+  //     if (err) throw err;
+  //     console.log("插入的languages数量为: " + res.insertedCount);
+  //   });
+  // }
+
+  dbConnect.close();
 }
 
 (async () => {
